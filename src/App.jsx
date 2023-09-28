75bd27f2273ce4f1b236309ee52de32b560e1ce6
pnoikid <126824718+pnoikid@users.noreply.github.com>
2023-09-28 16:53:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { useState, useEffect } from 'react'
-//import ProductItems from './components/ProductItems';
-import './index.css'
+import AllProducts from './components/AllProducts';
 import Login from './components/Login';
-
+import Cart from './components/Cart';
+import "./index.css"
 const App = () => {
   const [items, setItems] = useState([]);
 
-  useEffect (() => {
-    const getAllData = async () =>{
+  useEffect(() => {
+    const getAllData = async () => {
       try {
-        const response = await fetch ('https://fakestoreapi.com/products')
+        const response = await fetch('https://fakestoreapi.com/products')
         const results = await response.json()
         setItems(results);
-      } catch (error){
+      } catch (error) {
         console.error(error)
       }
     };
     getAllData();
-  },[]);
- 
-console.log (items);
+  }, []);
+
+  console.log(items);
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Store</h1>
       <Login />
       <ul>
         {items.map(item => (
-          <li key = {item.id}>{item.title}, {item.image}</li>
+          <li key={item.id}>{item.title}, {item.image}</li>
         ))}
       </ul>
 
