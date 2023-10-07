08689b23de92da4363d3c7726f5d9e035e22f306
pnoikid <126824718+pnoikid@users.noreply.github.com>
2023-10-07 16:16:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,52 @@
-import { useState, useEffect } from 'react'
-import AllProducts from './components/AllProducts';
-import Login from './components/Login';
-import Cart from './components/Cart';
-import "./index.css"
-import "./AllProducts.css"
+import React, { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import AllProducts from "./components/AllProducts";
+import Login from "./components/Login";
+import Cart from "./components/Cart";
+import SingleProduct from "./components/SingleProduct";
+import "./index.css";
+import "./AllProducts.css";
+
 const App = () => {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const getAllData = async () => {
-      try {
-        const response = await fetch('https://fakestoreapi.com/products')
-        const results = await response.json()
-        setItems(results);
-      } catch (error) {
-        console.error(error)
-      }
-    };
-    getAllData();
-  }, []);
-
-  console.log(items);
+  const [userToken, setUserToken] = useState(null);
+​
+  const handleLogout = () => {
+    setUserToken(null);
+  };
+​
   return (
     <div>
       <h1>Store</h1>
-      <Login />
-       <AllProducts />
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>{item.title}, {item.image}</li>
-        ))}
-      </ul>
-
-
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            {userToken ? (
+              <Link to="/" onClick={handleLogout}>
+                Logout
+              </Link>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<AllProducts />} />
+        <Route
+          path="/login"
+          element={<Login userToken={userToken} setUserToken={setUserToken} />}
+        />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products/:productId" element={<SingleProduct />} />
+      </Routes>
     </div>
-
-  )
-}
-
-export default App
+  );
+};
+​
+export default App;
